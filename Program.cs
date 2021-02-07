97e202545d698a5808c82d78369e1f32de1c0139
Ester Sousa <ester.albuquerque500@gmail.com>
2021-02-07 07:47:45 -0300

    Interface

--- Program.cs
+++ Program.cs
@@ -10,13 +10,30 @@ namespace Interface
             Carrinho carrinho = new Carrinho();
 
             Produto p1 = new Produto();
-
             p1.Codigo = 123;
-            p1.Nome = "Kimono";
-            p1.Preco = 100f;
+            p1.Nome = "KIMONO";
+            p1.Preco = 200f;
+
+            Produto p2 = new Produto();
+            p2.Codigo = 124;
+            p2.Nome = "LUVAS";
+            p2.Preco = 50f;
+
+            Produto p3 = new Produto();
+            p3.Codigo = 125;
+            p3.Nome = "FAIXAS";
+            p3.Preco = 35f;
 
             carrinho.Cadastrar(p1);
+            carrinho.Cadastrar(p2);
+            carrinho.Cadastrar(p3);
             carrinho.listar();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            carrinho.Alterar(p1.Codigo,p3);
+            carrinho.listar();
+            Console.ResetColor();
 
 
         }
